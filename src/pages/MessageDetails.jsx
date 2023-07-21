47f34be2983e84b0beba0f2bbe84a40eadd44073
Pratham <prathams@krishworks.com>
2023-07-21 20:31:52 +0530

    init routes and pages

---
+++ src/pages/MessageDetails.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function MessageDetails() {
+  return <div>MessageDetails</div>;
+}
---
+++ src/routes/Router.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "../pages/Home";
+import Inbox from "../pages/Inbox";
+import MessageDetails from "../pages/MessageDetails";
+import Spam from "../pages/Spam";
+import Trash from "../pages/Trash";
+
+export default function Router() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/mail">
+        <Route path="" element={<Home />} />
+        <Route path="inbox">
+          <Route path="" element={<Inbox />} />
+          <Route path=":id" element={<MessageDetails />} />
+        </Route>
+        <Route path="spam" element={<Spam />} />
+        <Route path="trash" element={<Trash />} />
+      </Route>
+    </Routes>
+  );
+}
