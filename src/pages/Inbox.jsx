4e8ff68cebafb3d3118ea10c034383d2db019a63
Pratham <prathams@krishworks.com>
2023-07-24 22:39:03 +0530

    animated sidebar

--- src/pages/Inbox.jsx
+++ src/pages/Inbox.jsx
@@ -4,31 +4,28 @@ export default function Inbox() {
   return (
     <div className="content">
       <h2 className="heading">Inbox</h2>
-      <fieldset
-        style={{
-          backgroundColor: "#FFF",
-          borderRadius: "5px",
-          padding: "10px",
-          paddingBottom: "20px",
-          width: "96%",
-        }}
-      >
+      <fieldset>
         <legend>Filters</legend>
-        <input
-          type="checkbox"
-          id="unread-email"
-          // checked={state.showUnread}
-          // onChange={() => dispatch({ type: "TOGGLE_SHOW_UNREAD" })}
-        />
-        <label htmlFor="unread-email">Show Unread Mails</label>
-
-        <input
-          type="checkbox"
-          id="starred-message"
-          // checked={state.showStarred}
-          // onChange={() => dispatch({ type: "TOGGLE_SHOW_STARRED" })}
-        />
-        <label htmlFor="starred-message">Show Starred Mails</label>
+        <div>
+          <div>
+            <input
+              type="checkbox"
+              id="unread-email"
+              // checked={state.showUnread}
+              // onChange={() => dispatch({ type: "TOGGLE_SHOW_UNREAD" })}
+            />
+            <label htmlFor="unread-email">Show Unread Mails</label>
+          </div>
+          <div>
+            <input
+              type="checkbox"
+              id="starred-message"
+              // checked={state.showStarred}
+              // onChange={() => dispatch({ type: "TOGGLE_SHOW_STARRED" })}
+            />
+            <label htmlFor="starred-message">Show Starred Mails</label>
+          </div>
+        </div>
       </fieldset>
       <h3> Unread: {0}</h3>
     </div>
