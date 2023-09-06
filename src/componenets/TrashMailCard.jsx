3615f0dc0ed1ba2f7875e4fbbd1a9bbef5ed4d81
Pratham <agrawalpratham2002@gmail.com>
2023-09-06 12:09:47 +0530

    added tash page

---
+++ src/componenets/TrashMailCard.jsx
@@ -0,0 +1,39 @@
+import { NavLink } from "react-router-dom";
+
+export default function TrashMailCard({ mail }) {
+  return (
+    <li
+      className="list-item"
+      style={{ backgroundColor: mail?.unread ? "#f2f6fc" : "" }}
+    >
+      <div className="list-heading">
+        <p>
+          <b>Subject : Lorem ipsum dolor sit amet. </b>
+        </p>
+        <button className="btn star">
+          Star
+          {/* {mail?.isStarred ? "Unstar" : "Star"} */}
+        </button>
+      </div>
+
+      <p className="para">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut aliquid
+        quasi repellat vel. Dignissimos beatae nemo tempore provident nostrum
+        labore numquam, temporibus enim, harum tenetur odio aliquid, explicabo
+        qui praesentium neque. Sit qui, animi nostrum vel velit laudantium amet?
+        Provident.
+      </p>
+
+      <div className="footer">
+        <button className="btn-detail">
+          <NavLink to={`/detail/${mail?.mId}`} style={{ color: "#3366CC" }}>
+            View Details
+          </NavLink>
+        </button>
+        <div className="btn-container">
+          <button className="btn restore">Restore</button>
+        </div>
+      </div>
+    </li>
+  );
+}
--- src/pages/Trash.jsx
+++ src/pages/Trash.jsx
@@ -1,5 +1,16 @@
 import React from "react";
+import TrashMailCard from "../componenets/TrashMailCard";
 
 export default function Trash() {
-  return <div>Trash</div>;
+  return (
+    <div className="content">
+      <h2 className="heading">Trash</h2>
+
+      <ul className="list">
+        {new Array(15).fill(1).map((e) => (
+          <TrashMailCard />
+        ))}
+      </ul>
+    </div>
+  );
 }
