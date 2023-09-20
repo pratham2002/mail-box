a5b8cd06eb905baee43450e02c0b2abe09dcb7c8
Pratham <agrawalpratham2002@gmail.com>
2023-09-20 12:52:34 +0530

    added message detail component

--- src/pages/MessageDetails.jsx
+++ src/pages/MessageDetails.jsx
@@ -1,5 +1,13 @@
 import React from "react";
+import DetailCard from "../componenets/DetailCard";
 
 export default function MessageDetails() {
-  return <div>MessageDetails</div>;
+  return (
+    <div className="content">
+      <h1 className="heading">Details</h1>
+      <ul className="list">
+        <DetailCard />
+      </ul>
+    </div>
+  );
 }
