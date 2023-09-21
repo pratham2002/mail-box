5a4b6eff5027258295f87cfd6f89d5e61572df6e
Pratham <agrawalpratham2002@gmail.com>
2023-09-21 09:56:14 +0530

    mail=box-ui compelted

--- src/pages/MessageDetails.jsx
+++ src/pages/MessageDetails.jsx
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
+import { useParams } from "react-router-dom";
 import DetailCard from "../componenets/DetailCard";
+import { MailContext } from "../context/MailContext";
 
 export default function MessageDetails() {
+  const { id } = useParams();
+
+  const { state } = useContext(MailContext);
+
+  console.log(state.mailData);
+  const mailDetail = state.mailData.find(({ mId }) => mId === id);
+  console.log({ mailDetail });
   return (
     <div className="content">
       <h1 className="heading">Details</h1>
       <ul className="list">
-        <DetailCard />
+        <DetailCard mail={mailDetail} />
       </ul>
     </div>
   );
