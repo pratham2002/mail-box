0da2195927d38ede0694f18aee62f072ce5c7bdb
Pratham <agrawalpratham2002@gmail.com>
2024-07-09 15:28:06 +0530

    updated pack.json

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import "./index.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <UserContextProvider>
         <MailContextProvider>
           <App />
